c194d8b2b116ac9efbe8527d1b5367d136b0cf66
dapeng126 <zhangdapeng89@126.com>
2016-09-25 12:21:27 +0800

    添加 日期 时间 对话框

--- app/src/main/java/com/gowhich/androiddialogs/MainActivity.java
+++ app/src/main/java/com/gowhich/androiddialogs/MainActivity.java
@@ -1,26 +1,46 @@
 package com.gowhich.androiddialogs;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button1;
     private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+
     private final CharSequence[] items = {"上海","北京","深圳"};
+
+    private int year;
+    private int monthOfYear;
+    private int dayOfMonth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        monthOfYear = calendar.get(Calendar.MONTH);
+        dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
         button1 = (Button) this.findViewById(R.id.button1);
         button2 = (Button) this.findViewById(R.id.button2);
+        button3 = (Button) this.findViewById(R.id.button3);
+        button4 = (Button) this.findViewById(R.id.button4);
+        button5 = (Button) this.findViewById(R.id.button5);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,6 +90,63 @@ public class MainActivity extends AppCompatActivity {
 
                 builder.create();
                 builder.show();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("请选择以下城市");
+                builder.setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        CharSequence selectItem = items[which];
+                        Toast.makeText(MainActivity.this, "--->" + selectItem + ' ' + which, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                builder.create();
+                builder.show();
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("请选择以下城市");
+                builder.setMultiChoiceItems(R.array.city, new boolean[]{false, false, false}, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+
+                    }
+                });
+
+                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                builder.create();
+                builder.show();
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        String str = year + "年" + month + "月" +dayOfMonth + "日";
+                        Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+                    }
+                }, year, monthOfYear, dayOfMonth);
+                datePickerDialog.show();
             }
         });
     }
